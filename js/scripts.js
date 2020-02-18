37e1881c52e55906b6927a290b476eb7d5d7dcc7
olivialimone <olivelemon@yahoo.com>
2020-02-18 17:31:17 -0500

    new edits

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoib2xpdmlhbGltb25lIiwiYSI6ImNrNmxmOXNqNzBlZnEzZG52M3dqdTF2anEifQ._jw03o430C3a-tly3N6-DQ';
 
 // this is the initial map focus when the user goes on the website
-var initialCenterPoint = [-95.7129, 37.0902]
-var initialZoom = 3
+var initialCenterPoint = [-122,45]
+var initialZoom = 2.5
 
 // create container to hold the initialization options for a mapboxGL map
 var initOptions = {
@@ -23,9 +23,9 @@ map.addControl(new mapboxgl.NavigationControl());
 unescoData.forEach(function(unescoSite) {
   // for each UNESCO site, add a marker to the map with a popup
   new mapboxgl.Marker()
-    .setLngLat([unescoSite.longitude, unescoEntry.latitude])
+    .setLngLat([unescoSite.longitude, unescoSite.latitude])
     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-     .setHTML(`${unescoSite.name}`))
+    .setHTML(`${unescoSite.name} <div><img src="${unescoSite.pic}"></div>`))
     .addTo(map);
 })
 
@@ -33,14 +33,14 @@ unescoData.forEach(function(unescoSite) {
 
 $('#west').on('click', function() {
   map.flyTo({
-    center: [-119.4179, 36.7783],
-    zoom: initialZoom
+    center: [-119.4179, 43.5],
+    zoom: 4
   })
 })
 
 $('#hawaii').on('click', function() {
 
-  var hawaiiLngLat = [-155.5828, 19.8968]
+  var hawaiiLngLat = [-163.041916, 20.828652]
 
   map.flyTo({
     center: hawaiiLngLat,
@@ -51,15 +51,15 @@ $('#hawaii').on('click', function() {
 $('#east').on('click', function() {
 
   map.flyTo({
-    center: [-77.8936, 34.0352],
-    zoom: 2
+    center: [-78.6569, 37.431],
+    zoom: 5
   })
 })
 
 $('#south').on('click', function() {
   map.flyTo({
     center: [-100, 31],
-    zoom: 2
+    zoom: 4
   })
 })
 
